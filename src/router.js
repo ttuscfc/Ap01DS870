85660e744b27f213a7cad55a23f5759c1a168fdc
Rafael Bini <rfabini1996@gmail.com>
2021-05-28 10:25:25 -0300

    Adicionei SearchClub e UpdateClub

--- src/router.js
+++ src/router.js
@@ -6,6 +6,46 @@ router.get("/clubs", (req, res) => {
     res.status(200).json(DB.clubs);
 });
 
+router.get("/searchClubs/:term", (req, res) => {
+    var clubs = DB.clubs.filter(c => c.name.toLowerCase().includes(req.params.term.toLowerCase()));
+    res.status(200).json(clubs);
+});
+
+router.put("/club/:id", (req, res) => {
+
+    var club = DB.clubs.find(c => c.id == req.params.id)
+
+    if (!club) {
+        res.status(400).json({ msg: "Nenhum time foi encontrado para esse ID" });
+        return;
+    }
+
+    const {
+        name,
+        city,
+        state,
+        division,
+        title,
+        payrool,
+    } = req.body;
+
+    if (division != undefined && division != "A" && division != "B" && division != "C" && division != "") {
+        res.status(400).json({ msg: "Insira a divisão corretamente com A, B, C ou deixe vazio." });
+        return;
+    }
+
+    if (name != undefined) club.name = name;
+    if (city != undefined) club.city = city;
+    if (state != undefined) club.state = state;
+    if (division != undefined) club.division = division;
+    if (title != undefined) club.title = title;
+    if (payrool != undefined) club.payrool = payrool;
+
+    res.status(200).json(club);
+
+
+});
+
 router.post("/newClub", (req, res) => {
     const {
         name,
@@ -15,8 +55,8 @@ router.post("/newClub", (req, res) => {
         title,
         payrool,
     } = req.body;
-    if (name && city && state && title && payrool != undefined){
-        if(division == "A" || division == "B" || division == "C" || division == ""){
+    if (name && city && state && title && payrool != undefined) {
+        if (division == "A" || division == "B" || division == "C" || division == "") {
             const id = DB.clubs.length + 1;
             DB.clubs.push({
                 id,
